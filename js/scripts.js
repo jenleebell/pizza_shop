d34e09efe99f442075f7f0ca1d9de5b15ad398bf
Jennifer <jenleebell@gmail.com>
2015-09-18 09:59:28 -0700

    Add pizza method to create a new pizza order

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+function Pizza (size, quantity) {
+  this.size = size;
+  this.quantity = quantity;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -2,6 +2,6 @@ describe("Pizza", function() {
   it("creates a new pizza order", function() {
     var testPizza = new Pizza("Large", 2);
     expect(testPizza.size).to.equal("Large");
-    expect(testAccount.quantity).to.equal(2);
+    expect(testPizza.quantity).to.equal(2);
   });
 });
